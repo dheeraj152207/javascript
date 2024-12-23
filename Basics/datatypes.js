a227fa9d3888b0059b0c465c29ab9a7d3868abe8
dheeraj152207 <71812181+dheeraj152207@users.noreply.github.com>
2024-12-23 19:37:01 +0000

    basic knowledge of datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treats all js codes as newer version
+// alert("hello") //no popup will can as we are using nodes, not browser
+
+/*
+variables in javascript:-
+1> number => range - 2^53
+2>bigint
+3>string = ""
+4>boolean = true/false
+5> null = standalone value (intentional absence of any object value)
+6> undefined = variable initialized but not allocated any value
+7> symbol
+8> object
+*/
+
+console.log(typeof null)   //output is object
+console.log(typeof undefined) //output is undefined
--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,15 @@
 console.log("om")
+// we have 3 types of variables in javascript let,const,var
+// but we use only const and let   (var have has function scope, not block scope, which can lead to unexpected behavior, such as variable hoisting and overwriting.)
+
+let email = "absc@gmail.com"
+const age = 22
+accountcity = "prayagraj"
+console.table([email,age,accountcity])
+
+// output
+// │ (index) │ Values           │
+// ├─────────┼──────────────────┤
+// │ 0       │ 'absc@gmail.com' │
+// │ 1       │ 22               │
+// │ 2       │ 'prayagraj'      │
