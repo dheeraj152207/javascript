4e37f11d5695dbfa9dffc7e197f2622db88af3df
dheeraj152207 <71812181+dheeraj152207@users.noreply.github.com>
2025-01-04 05:50:08 +0000

    object destructuring

---
+++ Basics/objects_destructuring.js
@@ -0,0 +1,11 @@
+const course = {
+    coursename:"js in hindi",
+    price : "999",
+    courseinstructor:"hitesh"
+}
+
+const {courseinstructor : instuctor} = course
+console.log(instuctor)
+
+// Object destructuring allows you to extract properties from an object and assign them to variables in a single,
+//  concise statemen
